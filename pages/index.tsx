31ca43a11176a5e196635c005a0f938c1cc14b29
1MarBur1 <artpivko@yandex.ru>
2022-04-09 12:34:56 +0500

    fix adaptive

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const MainPage = (): JSX.Element => {
 			<h1 className='font-bold text-3xl'>
 				Находка для шпиона / Локации
 			</h1>
-			<ul className='mt-8 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4'>
+			<ul className='mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4'>
 				{LOCATIONS.map((item, i) => (
 					<h2 key={i} className='font-semibold'>
 						{item}
