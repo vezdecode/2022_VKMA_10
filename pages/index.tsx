e5f5bd90128caaff0b79beb9eadd88fc1e4829c0
1MarBur1 <artpivko@yandex.ru>
2022-04-09 12:33:46 +0500

    first task done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import LOCATIONS from '../shared/consts/locations';
 
 const MainPage = (): JSX.Element => {
 	return(
-		<div>
-			VKMA10
+		<div className='p-8'>
+			<h1 className='font-bold text-3xl'>
+				Находка для шпиона / Локации
+			</h1>
+			<ul className='mt-8 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4'>
+				{LOCATIONS.map((item, i) => (
+					<h2 key={i} className='font-semibold'>
+						{item}
+					</h2>
+				))}
+			</ul>
 		</div>
 	);
 };
